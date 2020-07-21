5f920b2fb6fd15ded78ef881c6c9df1bcb0b273b
mikibaka <kaliszewski.mikolaj@gmail.com>
2020-07-21 21:55:33 +0200

    Updates

---
+++ precache-manifest.b4acf1f96ea234bdb4caf9ebb5efb245.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ca32c226dbbe1fc9d437908820c7b2",
+    "url": "/weather-app-task/index.html"
+  },
+  {
+    "revision": "3afb502a311990a2490d",
+    "url": "/weather-app-task/static/css/main.808cb233.chunk.css"
+  },
+  {
+    "revision": "4b1803b664f482e91c7e",
+    "url": "/weather-app-task/static/js/2.4ba4f759.chunk.js"
+  },
+  {
+    "revision": "174263e5b9d3e85c08d4329eaeeb7640",
+    "url": "/weather-app-task/static/js/2.4ba4f759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afb502a311990a2490d",
+    "url": "/weather-app-task/static/js/main.82baecd5.chunk.js"
+  },
+  {
+    "revision": "079d5d1279bebbdad95f",
+    "url": "/weather-app-task/static/js/runtime-main.e801983b.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/weather-app-task/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5f2f89b41c064007308461c032940b53",
+    "url": "/weather-app-task/static/media/header_background.5f2f89b4.jpg"
+  },
+  {
+    "revision": "006e1fc760776c9bd9f90e57e4e5acaa",
+    "url": "/weather-app-task/static/media/weather_200.006e1fc7.svg"
+  },
+  {
+    "revision": "ee36c6868a30f1f2067b1582e37e90fa",
+    "url": "/weather-app-task/static/media/weather_210.ee36c686.svg"
+  },
+  {
+    "revision": "811ba74fb75c28f163e6d4d22ccfa263",
+    "url": "/weather-app-task/static/media/weather_300.811ba74f.svg"
+  },
+  {
+    "revision": "bd5ad00507f12bdf45a900575db2e1ed",
+    "url": "/weather-app-task/static/media/weather_500.bd5ad005.svg"
+  },
+  {
+    "revision": "aa522a6c35263741f99b6c1d572b2d17",
+    "url": "/weather-app-task/static/media/weather_501.aa522a6c.svg"
+  },
+  {
+    "revision": "5c05e9065d6a73aef197d1ffc209d3ce",
+    "url": "/weather-app-task/static/media/weather_502.5c05e906.svg"
+  },
+  {
+    "revision": "17cafab0ce55d409ed3795e7081f09f8",
+    "url": "/weather-app-task/static/media/weather_600.17cafab0.svg"
+  },
+  {
+    "revision": "79d1c380efa55c92746aaf0a4de8ce2c",
+    "url": "/weather-app-task/static/media/weather_601.79d1c380.svg"
+  },
+  {
+    "revision": "0ecbe8be8a61a8b9aba746d768681323",
+    "url": "/weather-app-task/static/media/weather_700.0ecbe8be.svg"
+  },
+  {
+    "revision": "ea882d2c136a74f3bb6fd05b91985f39",
+    "url": "/weather-app-task/static/media/weather_800.ea882d2c.svg"
+  },
+  {
+    "revision": "1075e6dd8bb50234cbd659599210cd12",
+    "url": "/weather-app-task/static/media/weather_801.1075e6dd.svg"
+  },
+  {
+    "revision": "2fd75af0ca81c79f882df1c3f941a97e",
+    "url": "/weather-app-task/static/media/weather_803.2fd75af0.svg"
+  }
+]);
